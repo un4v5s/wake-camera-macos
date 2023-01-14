fc6bf3be62cf9c6790d693e452d57e67c36b5505
ooooo <un4v5s@gmail.com>
2023-01-14 18:14:21 +0900

    Initial Commit

---
+++ wake-camera/wake_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  wake_cameraApp.swift
+//  wake-camera
+//
+//  Created by Reona Ogino on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct wake_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
